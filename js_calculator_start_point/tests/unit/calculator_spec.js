8d2c1c9119d925399f2b56064e403a48ebe6bf22
Anna Cunningham <ac232@hotmail.co.uk>
2021-04-02 11:16:47 +0100

    first 4 unit tests passing

---
+++ js_calculator_start_point/tests/unit/calculator_spec.js
@@ -0,0 +1,44 @@
+var Calculator = require('../../public/js/calculator.js')
+var assert = require('assert')
+
+describe('calculator', function () {
+  beforeEach(function () {
+    calculator = new Calculator()
+  });
+
+  // write unit tests here in the form of "it should do something..."
+  it('it has a sample test', function(){
+    assert.strictEqual(true, true) //strict equal is like ===
+  })
+
+  // calculator.add() - add 1 to 4 and get 5
+  it('can add 1 to 4 and get 5', function(){
+    calculator.previousTotal = 1;
+    calculator.add(4);
+    assert.strictEqual(5, calculator.runningTotal)
+  })
+  // calculator.subtract() subtract 4 from 7 and get 3
+  it('can subtract 4 from 7 and get 3', function(){
+    calculator.previousTotal = 7;
+    calculator.subtract(4);
+    assert.strictEqual(3, calculator.runningTotal)
+  })
+  // calculator.multiply() - multiply 3 by 5 and get 15
+  it('can multiply 3 by 5 and get 15', function(){
+    calculator.previousTotal = 3;
+    calculator.multiply(5);
+    assert.strictEqual(15, calculator.runningTotal);
+  })
+  // calculator.divide() - divide 21 by 7 and get 3
+  // calculator.numberClick() - concatenate multiple number button clicks
+  // it('can concatenate multiple number button clicks', function(){
+  //   calculator.numberClick(1);
+  //   calculator.operatorClick("+")
+  //   calculator.numberClick(2);
+  //   calculator.operatorClick("=")
+  //   assert.strictEqual(12, calculator.runningTotal);
+  // })
+  // calculator.operatorClick() - chain multiple operations together
+  // calculator.clearClick() - clear the running total without affecting the calculation
+
+});
